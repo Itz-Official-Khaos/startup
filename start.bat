b2b734670bf97e58bc6916c94f81005da537f270
Itz-Official-Khaos <itzofficialkhaos@gmail.com>
2024-11-20 20:27:13 -0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,2 +1,13 @@
 timeout /t 30
-start SkibidiOT.exe
+@echo off
+:: Get the path of the Startup folder
+set startup_folder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
+
+:: Set the path of your SkibidiOT.exe file
+set exe_path=C:\path\to\SkibidiOT.exe
+
+:: Copy SkibidiOT.exe to the Startup folder
+copy "%exe_path%" "%startup_folder%\SkibidiOT.exe"
+
+:: Optionally, you can run the executable immediately (optional)
+start "" "%startup_folder%\SkibidiOT.exe"
